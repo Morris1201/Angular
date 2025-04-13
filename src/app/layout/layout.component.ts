b35b9c87cd04d688eb247d0c5f33f6f1ecd8e8db
morris <morris91re@gmail.com>
2025-04-13 14:49:45 +0800

    feat: ViewContainerRef

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,27 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, ViewContainerRef } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { CommonModule } from '@angular/common';
+import { TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
+  standalone: true,
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css'],
   imports: [HeaderComponent, FooterComponent, NzLayoutModule, NzDropDownModule, NzIconModule, CommonModule]
 })
 export class LayoutComponent implements OnInit {
-  classObj: object={
-    name: '小程序A計畫',
-    teacher: "Tom老師",
-    link: 'https://www.sikiedu.com/classroom/15/introduction'
-  }
+  @ViewChild('ref')
+  ref!: TemplateRef<any>
+  version: String="0.0.1"
+
+  @ViewChild('ref', {read:ViewContainerRef})
+  refContainer!: ViewContainerRef
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    // const view = this.ref.createEmbeddedView(null)
+    // const element = this.ref.elementRef.nativeElement
+
+    // view.rootNodes.forEach((node) => {
+    //   element.parentNode.insertBefore(node, element.nextSibling)
+    // })
+    const view = this.ref.createEmbeddedView(null)
+    this.refContainer.insert(view)
+  }
 }
